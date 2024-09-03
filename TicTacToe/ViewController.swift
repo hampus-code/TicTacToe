251eab34f3a9586edb9b2a968835a4db25366d81
hampus-code <hampusss96@gmail.com>
2024-09-03 10:50:40 +0200

    First commit

---
+++ TicTacToe/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  TicTacToe
+//
+//  Created by Hampus Andersson on 2024-09-03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var game = "Game"
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
