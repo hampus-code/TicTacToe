0002d3bd32f2b233ee7b5f25730bb9e4e28e9582
hampus-code <hampusss96@gmail.com>
2024-09-04 16:38:40 +0200

    Changed square outlets so that all square outlets are in one outlet collection. I also added a tag to every square

--- TicTacToe/Controllers/GameViewController.swift
+++ TicTacToe/Controllers/GameViewController.swift
@@ -9,15 +9,8 @@ import UIKit
 
 class GameViewController: UIViewController {
     
-    @IBOutlet weak var squareOne: UIImageView!
-    @IBOutlet weak var squareTwo: UIImageView!
-    @IBOutlet weak var squareThree: UIImageView!
-    @IBOutlet weak var squareFour: UIImageView!
-    @IBOutlet weak var squareFive: UIImageView!
-    @IBOutlet weak var squareSix: UIImageView!
-    @IBOutlet weak var squareSeven: UIImageView!
-    @IBOutlet weak var squareEight: UIImageView!
-    @IBOutlet weak var squareNine: UIImageView!
+    
+    @IBOutlet var boardSquares: [UIImageView]!
     
     @IBOutlet weak var scorePlayerOne: UILabel!
     @IBOutlet weak var scorePlayerTwo: UILabel!
@@ -26,21 +19,17 @@ class GameViewController: UIViewController {
     @IBOutlet weak var playerTwoCircle: UIImageView!
     
     
-    
     //To check which player's turn it is
     let PLAYER_ONE = 1
     let PLAYER_TWO = 2
     
-    /*
-    var gameBoard: [UIImageView: [Int]] = [
-        "square1" : [0,1,2]
-    ]
-     */
+     
         
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+
     }
 
 
