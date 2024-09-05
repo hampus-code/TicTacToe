5bfc9d0510306539adda0d6872cbf53c52216543
hampus-code <hampusss96@gmail.com>
2024-09-05 22:52:50 +0200

    Added Pan Gesture Recognizer to the X and the O ImageView and made functions them so i can drag the X and the O

--- TicTacToe/Controllers/GameViewController.swift
+++ TicTacToe/Controllers/GameViewController.swift
@@ -23,15 +23,80 @@ class GameViewController: UIViewController {
     let PLAYER_ONE = 1
     let PLAYER_TWO = 2
     
-     
-        
+    var initalXPosition: CGPoint = CGPoint.zero
+    var initalOPosition: CGPoint = CGPoint.zero
+    
+    //To check which board is which
+    var gameBoard = [0,1,2,3,4,5,6,7,8]
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        initalXPosition = playerOneCross.center
+        initalOPosition = playerTwoCircle.center
+        
+        /*
+        for (index, square) in boardSquares.enumerated() {
+            
+            print("Square: \(square.tag) at Index: \(index)")
+            
+        }
+         */
 
     }
-
-
+    
+    //Drag function
+    @IBAction func onDragX(_ sender: UIPanGestureRecognizer) {
+        
+        
+        let translation = sender.translation(in: self.view)
+        
+        playerOneCross.center = CGPoint(x: translation.x + playerOneCross.center.x, y: translation.y + playerOneCross.center.y)
+        
+        sender.setTranslation(CGPoint.zero, in: self.view)
+        
+        
+        if sender.state == .ended {
+            
+            //To check frames in the UIImageView outlet collections
+            for squares in boardSquares {
+                let frame = squares.frame
+                print("Frame of imageView: \(frame)")
+                
+                if squares.frame.contains(playerOneCross.frame) {
+                    sender.state = .ended
+                    
+                }
+                    }
+            
+            playerOneCross.center = initalXPosition
+        }
+        
+        
+    }
+    
+    
+    @IBAction func onDragO(_ sender: UIPanGestureRecognizer) {
+        
+        let translation = sender.translation(in: self.view)
+        
+        playerTwoCircle.center = CGPoint(x: translation.x + playerTwoCircle.center.x, y: translation.y + playerTwoCircle.center.y)
+        
+        sender.setTranslation(CGPoint.zero, in: self.view)
+        
+        
+        if sender.state == .ended {
+            
+            playerTwoCircle.center = initalOPosition
+            
+        }
+        
+        
+    }
+    
+        
 }
+    
 
